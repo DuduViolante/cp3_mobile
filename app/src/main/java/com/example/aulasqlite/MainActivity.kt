49207eec28583cdb574d8dbf5d8245208110ac19
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2024-09-30 11:48:29 -0300

    Adicionando as funções salvar, excluir, listar, atualizar - Banco de dados SQLite

--- app/src/main/java/com/example/aulasqlite/MainActivity.kt
+++ app/src/main/java/com/example/aulasqlite/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.aulasqlite
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,9 +13,15 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private val bancoDados by lazy{
+    private val bancoDados by lazy {
         DatabaseHelper(this)
     }
+
+    private lateinit var btnSalvar: Button
+    private lateinit var btnListar:Button
+    private lateinit var btnAtualizar:Button
+    private lateinit var btnDeletar:Button
+    private lateinit var editNomeProduto: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +33,81 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-       try{
-           val sql = "INSERT INTO produtos VALUES(null,'Celular Motorola','512gb')"
-           bancoDados.writableDatabase.execSQL(sql)
-           Log.i("db_info","Registro salvo")
-       }catch (e:Exception){
-           e.printStackTrace()
-       }
+        btnSalvar = findViewById(R.id.btnSalvar)
+        btnListar = findViewById(R.id.btnListar)
+        btnAtualizar = findViewById(R.id.btnAtualizar)
+        btnDeletar = findViewById(R.id.btnDeletar)
+        editNomeProduto = findViewById(R.id.editNomeProduto)
 
+        btnSalvar.setOnClickListener {
+            salvar()
+        }
+
+        btnListar.setOnClickListener{
+            listar()
+        }
+
+        btnAtualizar.setOnClickListener{
+            atualizar()
+        }
+
+        btnDeletar.setOnClickListener{
+            deletar()
+        }
+
+    }
+
+    private fun salvar() {
+        val nomeProduto = editNomeProduto.text.toString()
+
+        try {
+            val sql = "INSERT INTO produtos VALUES(null,'$nomeProduto','512gb')"
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("db_info", "Registro salvo")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun listar(){
+        val sql = "SELECT * FROM ${DatabaseHelper.TABELA_PRODUTOS}"
+        val cursor = bancoDados.readableDatabase.rawQuery(sql,null)
+
+        val indiceProduto = cursor.getColumnIndex("${DatabaseHelper.ID_PRODUTO}")
+        val indiceTitulo = cursor.getColumnIndex("${DatabaseHelper.TITULO}")
+        val indiceDescricao = cursor.getColumnIndex("${DatabaseHelper.DESCRICAO}")
+
+        while(cursor.moveToNext()){
+            val idProduto = cursor.getInt(indiceProduto)
+            val titulo = cursor.getString(indiceTitulo)
+            val descricao = cursor.getString(indiceDescricao)
+
+            Log.i("db_info","Produto: $idProduto - $titulo - $descricao")
+        }
+    }
+
+    private fun atualizar(){
+        val nomeProduto = editNomeProduto.text.toString()
+
+        try {
+            val sql = "UPDATE produtos SET titulo= '$nomeProduto' WHERE id_produto=2"
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("db_info", "Registro atualizado com sucesso")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i("db_info", "Registro não atualizado")
+        }
+    }
+
+    private fun deletar(){
+        val sql = "DELETE FROM produtos WHERE id_produto=2"
+
+        try {
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("db_info","Sucesso ao remover")
+        }catch (e:Exception){
+            e.printStackTrace()
+            Log.i("db_info","Error ao remover")
+        }
     }
 }
